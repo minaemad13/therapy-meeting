28d45cad61e3dd3cfcb5a6eaf1af8f8d500e83cc
usamaserag <usamaserageldeen@gmail.com>
2024-09-06 15:41:07 +0300

    style the form and add flags

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: [
+    './templates/**/*.html',
+    './**/templates/**/*.html',
+    './static/**/*.css',
+    './static/**/*.js',
+  ],
+  theme: {
+    colors: {
+      'blue': '#1fb6ff',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+    },
+  },
+  plugins: [],
+}
